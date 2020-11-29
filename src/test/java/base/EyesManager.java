fb54aace97399e1332de9bfe26725f0cd384092c
youvegotnigel <nigeldave19@gmail.com>
2020-11-29 13:21:48 +0530

    update eyes manager

--- src/test/java/base/EyesManager.java
+++ src/test/java/base/EyesManager.java
@@ -67,10 +67,10 @@ public class EyesManager {
 
         // Visual Grid configurations
         eyesConfig.addBrowser(1200, 800, BrowserType.CHROME);
-//        eyesConfig.addBrowser(1200, 800, BrowserType.FIREFOX);
-//        eyesConfig.addBrowser(1200, 800, BrowserType.EDGE_CHROMIUM);
-//        eyesConfig.addBrowser(1200, 800, BrowserType.SAFARI);
-//        eyesConfig.addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT);
+        eyesConfig.addBrowser(1200, 800, BrowserType.FIREFOX);
+        eyesConfig.addBrowser(1200, 800, BrowserType.EDGE_CHROMIUM);
+        eyesConfig.addBrowser(1200, 800, BrowserType.SAFARI);
+        eyesConfig.addDeviceEmulation(DeviceName.iPhone_X, ScreenOrientation.PORTRAIT);
         //eyesConfig.setTestName("Test 1");
 
     }
